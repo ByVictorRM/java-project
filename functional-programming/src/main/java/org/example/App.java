9975ed9ade391995564cf9064c9f19992df7eaca
VictorRodriguezContact <victorrodriguezcontact@gmail.com>
2025-09-02 10:14:32 +0100

    Build success here!

--- functional-programming/src/main/java/org/example/App.java
+++ functional-programming/src/main/java/org/example/App.java
@@ -11,13 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Lambdas and method references:
-        // Lambda
-        Comparator<String> byLength = (a, b) -> Integer.compare(a.length(), b.length());
-
-// Method reference
-        List<String> names = List.of("Ana","Luis","Sofía","Pablo");
-        names.sort(Comparator.comparingInt(String::length));
+        
         System.out.println( "Hello World!" );
     }
 }
